427cc8313adaa21d93adc1a01ceb5682959640e1
Jure Cuhalev <gandalfar@gmail.com>
2011-02-12 21:45:28 +0100

    more buildout magic

--- future_todo/__init__.py
+++ future_todo/__init__.py
@@ -1,4 +1,7 @@
+from django.core.management import setup_environ
+from future_todo import settings
+setup_environ(settings)
 from t3p.views import plugin_handler
-from django.conf import settings
+#from django.conf import settings
 
 settings.plugin_handler = plugin_handler
--- future_todo/settings.py
+++ future_todo/settings.py
@@ -72,6 +72,7 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'future_todo.urls'
 
+print PROJECT_DIR
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
